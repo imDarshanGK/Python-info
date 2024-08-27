8ddf5fcae8372e009cf32cc3aa91ece95ad34856
Darshan G K <122042809+imDarshanGK@users.noreply.github.com>
2024-08-27 06:57:42 +0530

    Update username_password.py

--- Python password/username_password.py
+++ Python password/username_password.py
@@ -1,4 +1,4 @@
 from getpass import getpass
 username = input("Enter Username: ")
 password = getpass("Enter password: ")
-print(password)
+#print(password) If it shows the password, then use the print statement
