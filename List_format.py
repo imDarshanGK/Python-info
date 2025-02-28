c933a904ddca267a4e9229fe46f0e629285a4706
Darshan G K <122042809+imDarshanGK@users.noreply.github.com>
2025-02-28 21:47:45 +0530

    Add new features: display last two characters and list length

--- List_format.py
+++ List_format.py
@@ -1,3 +1,6 @@
 List = list('PYTHON')
 values = (List[0], List[1:3]) 
-print('v1={0}, v2={1}'.format(*values))
+last_two = List[-2:]  # Taking the last two characters
+list_length = len(List)  # Getting the length of the list
+
+print('v1={0}, v2={1}, last_two={2}, list_length={3}'.format(*values, last_two, list_length))
