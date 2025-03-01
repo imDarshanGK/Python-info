4b16c7bff97d1a6b578c6e0129ad9dbe9638177e
Darshan G K <122042809+imDarshanGK@users.noreply.github.com>
2025-03-01 20:50:32 +0530

    Added money hiding game with user input for position selection

--- Money-hider-game.py
+++ Money-hider-game.py
@@ -1,11 +1,16 @@
 row1 = ['🤑', '🤑', '🤑']
 row2 = ['🤑', '🤑', '🤑']
 row3 = ['🤑', '🤑', '🤑']
-selected_rows = [row1,row2,row3]
+selected_rows = [row1, row2, row3]
+
 print(f'{row1}\n{row2}\n{row3}\n')
-postion = input("Enter the postion where you want to hide the money: ")
-row_numner = int(postion[0])
-coulum_number = int(postion[1])
-row_selected = selected_rows[row_numner-1]
-row_selected[coulum_number-1] = "X"
+
+position = input("Enter the position where you want to hide the money (row and column as two digits, e.g., 12 for row 1 column 2): ")
+
+row_number = int(position[0])
+column_number = int(position[1])
+
+row_selected = selected_rows[row_number - 1]
+row_selected[column_number - 1] = "X"
+
 print(f'{row1}\n{row2}\n{row3}\n')
