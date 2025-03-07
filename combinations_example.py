8e1e9d187b32f2c6216606e4a432720bb48a7bfc
imDarshanGK <darshan241210s@gmail.com>
2025-03-07 20:49:57 +0530

    Added feature to generate combinations with repetition

--- combinations_example.py
+++ combinations_example.py
@@ -1,4 +1,9 @@
 import itertools
 
+# Generate Combinations without Repetition
 result = list(itertools.combinations([1, 2, 3], 2))
 print(result)
+
+# Generate Combinations with Repetition
+result_with_replacement = list(itertools.combinations_with_replacement([1, 2, 3], 2))
+print(result_with_replacement)
