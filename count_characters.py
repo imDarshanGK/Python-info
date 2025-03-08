82186dd1d8db2754bff44f3c0460f14c8357b85f
imDarshanGK <darshan241210s@gmail.com>
2025-03-08 21:28:52 +0530

    Add a new feature to count characters in a user-provided string and get the most common elements.

--- count_characters.py
+++ count_characters.py
@@ -5,3 +5,9 @@ counter = collections.Counter("aabbccc")
 
 # Get the two most common elements
 print(counter.most_common(2))
+
+# New feature: Count characters in a user-provided string and get the most common elements
+user_string = input("Enter a string: ")
+user_counter = collections.Counter(user_string)
+most_common_elements = user_counter.most_common(2)
+print(f"The two most common elements in the provided string are: {most_common_elements}")
