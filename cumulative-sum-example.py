731557981a51b85a9c9e333c8d09cb63fd2ba7d7
Darshan G K <122042809+imDarshanGK@users.noreply.github.com>
2025-03-11 20:46:37 +0530

    Add cumulative sum calculation with index printing

--- cumulative-sum-example.py
+++ cumulative-sum-example.py
@@ -1,3 +1,5 @@
-a=[1,2,3,4]
-b=[sum(a[0:x+1]) for x in range(0,len(a))] 
-print(b)
+a = [1, 2, 3, 4]
+b = [sum(a[0:x+1]) for x in range(0, len(a))]
+
+for i, value in enumerate(b):
+    print(f"Index {i}: Cumulative sum = {value}")
