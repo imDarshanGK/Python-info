02b88c6e9e7088a1ff74bdc30c193fd7be412eb2
Darshan <darshan241210s@gmail.com>
2025-03-24 20:53:28 +0530

    Added string reversal functionality

---
+++ Hello world.py
@@ -0,0 +1,11 @@
+# Reverse the string "Hello World!" and print it
+print("Hello World!"[::-1])  
+# Output: !dlroW olleH
+
+# Join and reverse the string
+joined_string = ', '.join(['apple', 'banana', 'cherry'])
+print(joined_string)  # Output: apple, banana, cherry
+
+# Reverse the joined string
+reversed_joined_string = joined_string[::-1]
+print(reversed_joined_string)  # Output: yrrehc ,ananab ,elppa
