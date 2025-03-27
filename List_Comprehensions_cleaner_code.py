f6447a9c8c1d9332be90528dcf50e3ae50032efc
Darshan <darshan241210s@gmail.com>
2025-03-27 22:10:15 +0530

    Add squares and cubes calculation with separate lists and tuples

--- List_Comprehensions_cleaner_code.py
+++ List_Comprehensions_cleaner_code.py
@@ -1,4 +1,10 @@
-squares = []
-for i in range(10):
-  squares.append(i**2)
 squares = [i**2 for i in range(10)]
+cubes = [i**3 for i in range(10)]
+
+# Generating a list of tuples with both squares and cubes
+squares_and_cubes = [(i**2, i**3) for i in range(10)]
+
+# Printing the results
+print("Squares:", squares)
+print("Cubes:", cubes)
+print("Squares and Cubes (as tuples):", squares_and_cubes)
