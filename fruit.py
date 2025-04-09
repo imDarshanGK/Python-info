3804477a123ec94e905cad65fa3463be09ce5fba
Darshan G K <122042809+imDarshanGK@users.noreply.github.com>
2025-04-09 21:01:55 +0530

    Added color attribute and display_color method to Apple class

--- fruit.py
+++ fruit.py
@@ -1,9 +1,16 @@
 class Fruit:
     def __init__(self):
-        print('1') 
+        print('1')
 
 class Apple(Fruit):
-    def __init__(self):
+    def __init__(self, color="Red"):
         super().__init__()
+        self.color = color
         print('2')
+
+    def display_color(self):
+        print(self.color)
+
+# Create an Apple object
 obj = Apple()
+obj.display_color()  # Output: Red
