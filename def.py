43cfecb26bdf9b5024adc4e8404dba35183a5da6
Darshan G K <122042809+imDarshanGK@users.noreply.github.com>
2025-04-15 22:55:28 +0530

    feat: add combined string output and even check to fun()

--- def.py
+++ def.py
@@ -1,5 +1,9 @@
 def fun(x):
-  a = "Py"
-  x = x ** 3
-  return a, x
-fun(4)
+    a = "Py"
+    x = x ** 3
+    combined = a + str(x)
+    is_even = (x % 2 == 0)
+    return combined, x, is_even
+
+result = fun(4)
+print(result)
