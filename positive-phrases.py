34315fd33b2879fc378009e70817e3d7a42fd8cd
Darshan G K <122042809+imDarshanGK@users.noreply.github.com>
2025-05-08 22:03:26 +0530

    feat: allow user to input phrase and display word at specified index

--- positive-phrases.py
+++ positive-phrases.py
@@ -1,2 +1,9 @@
-phrase = "Have a nice day"
-print(phrase.split(" ")[2])
+phrase = input("Enter a phrase: ")
+index = int(input("Enter the index of the word to display (starting from 0): "))
+
+words = phrase.split(" ")
+
+if 0 <= index < len(words):
+    print(f"The word at index {index} is: {words[index]}")
+else:
+    print("Index out of range.")
