6b6dcf24553995802d0440b5b8dc8daf8974750f
Darshan G K <122042809+imDarshanGK@users.noreply.github.com>
2025-05-11 21:55:15 +0530

    Add check for even or odd sum of the original list

--- reverse-list-access.py
+++ reverse-list-access.py
@@ -1,3 +1,10 @@
 List = [1, 0, 2]
+
 a, b, c = List[::-1]
+
+if sum(List) % 2 == 0:
+    print("The sum of the original list is even.")
+else:
+    print("The sum of the original list is odd.")
+
 print(List[b])
