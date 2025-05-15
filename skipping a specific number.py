9c2fadbe5997aefb2cd157aefb32044d299ea89e
Darshan G K <122042809+imDarshanGK@users.noreply.github.com>
2025-05-15 21:35:23 +0530

    Add logging for skipped and terminated steps in loop

--- skipping a specific number.py
+++ skipping a specific number.py
@@ -1,6 +1,8 @@
 for i in range(1, 11):
-  if i == 3:
-    continue
-  if i == 7:
-    break
-  print(i)
+    if i == 3:
+        print("Skipped number 3")
+        continue
+    if i == 7:
+        print("Terminated at number 7")
+        break
+    print(i)
