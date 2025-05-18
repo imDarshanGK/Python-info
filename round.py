59b7c7e7e79cdc6b8418153b82dafc2a3d39b9d9
Darshan G K <122042809+imDarshanGK@users.noreply.github.com>
2025-05-18 20:30:15 +0530

    Calculate and display the square of the rounded number.

--- round.py
+++ round.py
@@ -1,3 +1,5 @@
-number = 28/6
+number = 28 / 6
 round_num = round(number, 2)
-print(round_num)
+square_of_num = round_num ** 2
+print(f"Rounded number: {round_num}")
+print(f"Square of the rounded number: {square_of_num}")
